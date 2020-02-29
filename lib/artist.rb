7628cecfb069b01816fd602b81ac28c7252331b3
G. Vanessa Fotso <vanessuniq@gmail.com>
2020-02-29 06:13:44 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -9,5 +9,23 @@ class Artist
   attr_accessor :name
   attr_reader :songs
 
+  @@artists = []
   
+  def self.all
+    @@artists
+  end
+
+  def initialize
+    super
+    @songs = []
+  end
+
+  def add_song(song)
+    @songs << song
+    song.artist = self unless song.artist
+  end
+
+  def add_songs(songs)
+    songs.each { |song| add_song(song) }
+  end
 end
