3aefb8594f5cbbb70343a283b7892efe73b3ce6c
G. Vanessa Fotso <vanessuniq@gmail.com>
2020-02-29 06:14:56 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -9,25 +9,5 @@ class Song
   attr_accessor :name
   attr_reader :artist
 
-  @@songs = []
-
-  def initialize
-    @@songs << self
-  end
-
-  def self.find_by_name(name)
-    @@songs.detect{|a| a.name == name}
-  end
-
-  def self.all
-    @@songs
-  end
-
-  def artist=(artist)
-    @artist = artist
-  end
-
-  def to_param
-    name.downcase.gsub(' ', '-')
-  end
+  
 end
