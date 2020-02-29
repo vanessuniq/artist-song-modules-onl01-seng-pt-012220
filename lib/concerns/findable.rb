99d83bc09df39c3e7e6ed55664437968d82150cf
G. Vanessa Fotso <vanessuniq@gmail.com>
2020-02-29 05:58:10 +0000

    Automatically backed up by Learn

--- lib/concerns/findable.rb
+++ lib/concerns/findable.rb
@@ -1,5 +1,7 @@
 module Findable
-  def find_by_name(name)
-    all.detect {|a| a.name == name}
+  Module ClassMethods
+    def find_by_name(name)
+      all.detect {|a| a.name == name}
+    end
   end
 end
