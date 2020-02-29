e831325fd32b1283b93dc215a049f82d23c33909
G. Vanessa Fotso <vanessuniq@gmail.com>
2020-02-29 05:51:41 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,6 +1,10 @@
 require 'pry'
 
 class Artist
+  extend Memorable::ClassMethods
+  extend Findable::ClassMethods
+  include Memorable::InstanceMethods
+  include Paramable::InstanceMethods
   
   attr_accessor :name
   attr_reader :songs
